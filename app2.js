0b786ae68cdbcf9d387d41b3c03c46f41c0f6be1
Yaroslav Avtonomov <avtnmv@icloud.com>
2024-08-02 01:44:05 +0300

    first commit

---
+++ app2.js
@@ -0,0 +1,6 @@
+const container = document.getElementById('container');
+for (let i = 0; i < 900; i++) {
+    const div = document.createElement('div');
+    div.className = 'c';
+    container.appendChild(div);
+}
